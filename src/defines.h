05491dfc01b2c00d57f7502a310749b8fc3bb1b1
TheBug233 <33407430+TheBug233@users.noreply.github.com>
2023-03-08 01:54:12 +0800

    Update defines.h
    Modified so that it can be used directly

--- src/defines.h
+++ src/defines.h
@@ -26,11 +26,11 @@
 // ================================================
 
 // Set parameters of IMU and board used
-#define IMU IMU_BNO085
-#define SECOND_IMU IMU
-#define BOARD BOARD_SLIMEVR
-#define IMU_ROTATION DEG_90
-#define SECOND_IMU_ROTATION DEG_270
+#define IMU IMU_MPU9250
+#define SECOND_IMU IMU_MPU9250
+#define BOARD BOARD_NODEMCU
+#define IMU_ROTATION DEG_180
+#define SECOND_IMU_ROTATION DEG_90
 
 // Battery monitoring options (comment to disable):
 //   BAT_EXTERNAL for ADC pin, 
